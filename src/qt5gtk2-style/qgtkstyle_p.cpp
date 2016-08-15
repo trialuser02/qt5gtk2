73afe3caa509d2ef9e1b6a4e8e4c5a208da14e0e
trialuser <forkotov02@hotmai.ru>
2016-08-15 14:39:07 +0300

    fixed copyright headers

--- src/qt5gtk2-style/qgtkstyle_p.cpp
+++ src/qt5gtk2-style/qgtkstyle_p.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2015 The Qt Company Ltd.                                *
- *   Copyright (C) 2016 Ilya Kotov, forkotov02@hotmail.ru                  *                               *
+ *   Copyright (C) 2016 Ilya Kotov, forkotov02@hotmail.ru                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
