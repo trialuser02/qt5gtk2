7bff23dfbbf9128bda073f17e020d828593e9c07
trialuser <forkotov02@hotmai.ru>
2016-08-15 11:07:52 +0300

    added dirview test

---
+++ tests/dirview/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+MYDIR=$(dirname $0)
+
+#export QT_DEBUG_PLUGINS=1
+export QT_QPA_PLATFORMTHEME=qt5gtk2
+#export QT_QPA_PLATFORM_PLUGIN_PATH=$(dirname $0)/../../qt5gtk2/src/qt5gtk2-qtplugin
+$MYDIR/dirview
